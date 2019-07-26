8d4e173d841913e1f077fce1b8046a7689565e65
Test Student <alex.bokov@gmail.com>
2019-07-26 21:19:44 +0000

    New file for setting up local git

---
+++ git_setup.R
@@ -0,0 +1,12 @@
+# Set up user.name and user.email
+
+if(!is.null(attr(system('git config --global user.name',intern=T),'status'))){
+  .username <- readline('Please type in the user name you want to use and hit enter: ');
+  system(sprintf('git config --global user.name "%s"',.username));
+}
+
+if(!is.null(attr(system('git config --global user.email',intern=T),'status'))){
+  .useremail <- readline('Please type in the email address you want to use and hit enter: ');
+  system(sprintf('git config --global user.email "%s"',.useremail));
+}
+
