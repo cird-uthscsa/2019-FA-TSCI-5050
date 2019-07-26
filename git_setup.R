18111ad6fb317b3763fba2b090d84e8c1049aa50
Test Student <alex.bokov@gmail.com>
2019-07-26 21:28:23 +0000

    Adding useful aliases, proper section headings

--- git_setup.R
+++ git_setup.R
@@ -1,4 +1,4 @@
-# Set up user.name and user.email
+# Set up user.name and user.email ----
 
 if(!is.null(attr(system('git config --global user.name',intern=T),'status'))){
   .username <- readline('Please type in the user name you want to use and hit enter: ');
@@ -10,3 +10,7 @@ if(!is.null(attr(system('git config --global user.email',intern=T),'status'))){
   system(sprintf('git config --global user.email "%s"',.useremail));
 }
 
+# Set up useful aliases ----
+system('git config --global alias.upd "submodule update --init --recursive --remote"');
+system('git config --global alias.lg "log --oneline --graph --color --all --decorate"');
+c()
